70efc3d179167ce8a96d453e4de811ca4784a563
Koutarou ISHIKAWA <stein2nd@gmail.com>
2025-12-08 20:15:14 +0900

    Update ContentView.swift

--- CozyBrewApp/ContentView.swift
+++ CozyBrewApp/ContentView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import AppKit
 import CozyBrewService
 import CozyBrewUIComponents
 
@@ -19,6 +20,18 @@ struct ContentView: View {
             // S2J About Window を表示
             // 注: S2JAboutWindow の実際の API に合わせて調整が必要
             AboutWindowView()
+        }
+        .onAppear {
+            // ウィンドウサイズを設定し、前面に表示
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                if let window = NSApplication.shared.windows.first {
+                    window.setContentSize(NSSize(width: 1000, height: 700))
+                    window.center()
+                    window.makeKeyAndOrderFront(nil)
+                    window.orderFrontRegardless()
+                    NSApplication.shared.activate(ignoringOtherApps: true)
+                }
+            }
         }
     }
 }
